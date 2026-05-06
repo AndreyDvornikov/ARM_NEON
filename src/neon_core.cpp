a54be8368bd436584babb3cf4c4c3bbeb87b5f1e
DvornikovAA404 <advornikov0809@gmail.com>
2026-05-07 00:21:33 +0700

    Добавлены реализации Scalar и NEON

---
+++ src/neon_core.cpp
@@ -0,0 +1,24 @@
+#include <arm_neon.h>
+#include <cstdint>
+
+int32x4_t neon_process_chunk(const int32_t* data) {
+    int32x4_t vec = vld1q_s32(data);
+
+    int32x4_t zero = vdupq_n_s32(0);
+
+    int32x4_t mask_pos = vcgtq_s32(vec, zero);
+    int32x4_t mask_neg = vcltq_s32(vec, zero);
+
+    int32x4_t sign = vshrq_n_s32(vec, 31);
+
+    int32x4_t abs_vec = veorq_s32(vec, sign);
+    abs_vec = vsubq_s32(abs_vec, sign);
+
+    int32x4_t pos_part = vandq_s32(vec, mask_pos);
+
+    int32x4_t neg_part = vandq_s32(abs_vec, mask_neg);
+
+    int32x4_t result = vorrq_s32(pos_part, neg_part);
+
+    return result;
+}
---
+++ src/scalar.cpp
@@ -0,0 +1,23 @@
+#include <cstdint>
+#include <cstddef>
+
+int64_t process_array_scalar(const int32_t* data, size_t n)
+{
+    int64_t sum = 0;
+
+    for (size_t i = 0; i < n; ++i)
+    {
+        int32_t val = data[i];
+
+        if (val > 0)
+        {
+            sum += val;
+        }
+        else if (val < 0)
+        {
+            sum -= val;
+        }
+    }
+
+    return sum;
+}
